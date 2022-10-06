8254b834c88bccfa71bb97cf30e4326d31d0157b
Travis Okonicha <okonicha.ugo@gmail.com>
2022-10-06 15:42:15 +0100

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_ix/smart_ix/infrastructure/authentication/firebase_auth.dart';
@@ -20,3 +22,9 @@ void main() async {
     routineRepository: routineRepository,
   ));
 }
+
+// Future<UiState> blackBerry() async {
+//   var clag = Isolate.spawn((message) {}, '');
+//   ReceivePort();
+
+// }
